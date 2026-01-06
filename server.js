8f3110ccb626d0b631fbd243a64459eb3d92674c
tushar-karn <tricksburp@gmail.com>
2026-01-06 11:29:32 +0530

    Added User Schema

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
 require('dotenv').config();
 
 const PORT = process.env.PORT || 4000;
 
+const connectDB = require("./config/db");
+connectDB();
+
+app.get('/', (req, res) => {
+  res.send('Smart Inventory Backend is running');
+});
 
 
 app.listen(PORT, () => {
